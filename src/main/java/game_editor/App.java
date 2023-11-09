247baa8d211ef4a4a4b58ab801148ef07843d4e9
Schnur, Tim (DI PA DE-L DEV) <tim.schnur@siemens.com>
2023-11-09 11:50:39 +0100

    Klasse zum starten des Game Editors

---
+++ src/main/java/game_editor/App.java
@@ -0,0 +1,14 @@
+package game_editor;
+
+import game_editor.view.Frame;
+
+/**
+ * Startet den Game Editor
+ * 
+ * @author Tim Schnur
+ */
+public class App {
+    public static void main(String[] args) {
+        new Frame();
+    }
+}
