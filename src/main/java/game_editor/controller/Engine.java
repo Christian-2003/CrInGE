f48552637ddee78d1f8976e4a7a8cc0d9230f264
Christian-2003 <80711177+Christian-2003@users.noreply.github.com>
2023-11-12 13:14:39 +0100

    Added GameKeyEvent and KeyEventManager for keyboad events

--- src/main/java/game_editor/controller/Engine.java
+++ src/main/java/game_editor/controller/Engine.java
@@ -1,13 +1,27 @@
 package game_editor.controller;
 
+import game_engine.controller.GameKeyEvent;
+import game_engine.controller.KeyEventManager;
 import game_engine.controller.math_engine.EMatrix;
 import game_engine.controller.math_engine.MathException;
 import game_engine.view.Frame;
 
+import java.awt.event.KeyEvent;
+
 public class Engine {
     
     public static void main(String[] args) throws NullPointerException, MathException {
-        Frame frame = new Frame("Hello World");
+        Frame frame = new Frame("Main Frame");
+        KeyEventManager keyEventManager = new KeyEventManager();
+        keyEventManager.add(KeyEvent.VK_UP, keyCode -> {
+            System.out.println("UP");
+        });
+        keyEventManager.add(KeyEvent.VK_DOWN, keyCode -> {
+            System.out.println("DOWN");
+        });
+        keyEventManager.add(KeyEvent.VK_DOWN, keyCode -> {
+            System.out.println("DOWN2");
+        });
     }
     
 }
