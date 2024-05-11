ba59579ecf9c1511cbb39b59112214981461e9d1
FabianDev001 <52001566+FabianDev001@users.noreply.github.com>
2024-05-11 18:20:12 +0200

    🚨 Updated GameDataSyntaxException
    The Exception can now throw the trace of the datasyntax that is wrong, for faster debugging

--- src/main/java/game_engine/controller/game_datahandler/Exceptions/GameDataFileSyntaxException.java
+++ src/main/java/game_engine/controller/game_datahandler/Exceptions/GameDataFileSyntaxException.java
@@ -1,5 +1,7 @@
 package game_engine.controller.game_dataHandler.Exceptions;
 
+import java.nio.file.Path;
+
 /**
  * An Exception for when the GameData File Syntax has encountered any issues
  * Like from Format or wrong Syntax
@@ -12,4 +14,12 @@ public class GameDataFileSyntaxException extends Exception {
         super(error);
     }
 
+    /**
+     * If a Strace needs to be added to the Exception
+     * @param error | String
+     * @param path | Path as String
+     */
+    public GameDataFileSyntaxException(String error, String path){
+        super(error, new Throwable(path));
+    }
 }
